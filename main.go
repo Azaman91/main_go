c71429efb115a616fee824afcec7544cb25e726e
Azaman91 <artemprudnikov23@gmail.com>
2025-07-05 14:16:08 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	"errors"
-	"unicode/utf8"
+	"fmt"
 )
 
-var ErrInvalidUTF8 = errors.New("invalid utf8")
-
-func GetUTFLength(input []byte) (int, error) {
-	if !utf8.Valid(input) {
-		return 0, ErrInvalidUTF8
-	}
-
-	return utf8.RuneCount(input), nil
+type Animal interface {
+	MakeSound() string
+	GetName() string
+	GetInfo() string
 }
+
+type animal struct {
+	name    string
+	species string
+	age     int
+	sound   string
+}
+
+func (a animal) MakeSound() string {
+	return a.sound
+}
+
+func (a animal) GetName() string {
+	return a.name
+}
+
+func (a animal) GetInfo() string {
+	return fmt.Sprintf("Имя: %s, Вид: %s, Возраст: %d\n", a.name, a.species, a.age)
+}
+
+func NewAnimal(name, species string, age int, sound string) animal {
+	return animal{name: name, species: species, age: age, sound: sound}
+}
+
+func ZooShow(animals []Animal) {
+	for _, num := range animals {
+		fmt.Print(num.GetInfo())
+		fmt.Println(num.MakeSound())
+	}
+}
+
