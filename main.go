af0fdf6051b1cc48f1c8e7a64180aa0c538ac39d
Azaman91 <artemprudnikov23@gmail.com>
2025-07-05 14:27:01 +0300

    2 пет проект

--- main.go
+++ main.go
@@ -2,41 +2,75 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
 
-type Animal interface {
-	MakeSound() string
-	GetName() string
-	GetInfo() string
+type WordCount struct {
+	Word  string
+	Count int
 }
 
-type animal struct {
-	name    string
-	species string
-	age     int
-	sound   string
-}
+func getTopWords(wordMap map[string]int, n int) []string {
+	// 1. Преобразуем мапу в слайс структур WordCount
+	var wordCounts []WordCount
+	for word, count := range wordMap {
+		wordCounts = append(wordCounts, WordCount{word, count})
+	}
 
-func (a animal) MakeSound() string {
-	return a.sound
-}
+	// 2. Сортируем слайс по убыванию частоты
+	sort.Slice(wordCounts, func(i, j int) bool {
+		// Если частоты равны, сортируем по алфавиту
+		if wordCounts[i].Count == wordCounts[j].Count {
+			return wordCounts[i].Word < wordCounts[j].Word
+		}
+		return wordCounts[i].Count > wordCounts[j].Count
+	})
 
-func (a animal) GetName() string {
-	return a.name
-}
+	// 3. Берем первые N элементов
+	var topWords []string
+	for i := 0; i < n && i < len(wordCounts); i++ {
+		topWords = append(topWords, wordCounts[i].Word)
+	}
 
-func (a animal) GetInfo() string {
-	return fmt.Sprintf("Имя: %s, Вид: %s, Возраст: %d\n", a.name, a.species, a.age)
+	return topWords
 }
+func AnalyzeText(text string) {
+	lower := strings.ToLower(text)
+	p := strings.Map(func(r rune) rune {
+		if r == '.' || r == '!' || r == '?' || r == ',' {
+			return ' '
+		}
+		return r
+	}, lower)
+	words := strings.Fields(p)
 
-func NewAnimal(name, species string, age int, sound string) animal {
-	return animal{name: name, species: species, age: age, sound: sound}
-}
+	mapa := make(map[string]bool)
 
-func ZooShow(animals []Animal) {
-	for _, num := range animals {
-		fmt.Print(num.GetInfo())
-		fmt.Println(num.MakeSound())
+	for _, num := range words {
+		mapa[num] = true
+	}
+
+	wordCount := make(map[string]int)
+	for _, num := range words {
+		wordCount[num]++
+	}
+
+	var clovo string
+	max := 0
+	for word, count := range wordCount {
+		if count > max || (count == max && word < clovo) {
+			clovo = word
+			max = count
+		}
+	}
+
+	fmt.Printf("Количество слов: %d\n", len(words))
+	fmt.Printf("Количество уникальных слов: %d\n", len(mapa))
+	fmt.Printf("Самое часто встречающееся слово: \"%s\" (встречается %d раз)\n", clovo, max)
+	fmt.Printf("Топ-5 самых часто встречающихся слов:\n")
+	top5 := getTopWords(wordCount, 5)
+	for _, num := range top5 {
+		fmt.Printf("\"%s\": %d раз\n", num, wordCount[num])
 	}
 }
-
